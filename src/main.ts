fa21827fa8f2a3234de0a12e38878cc44c79f24c
arbinger <thomas.iniguez+github@free.fr>
2025-01-04 13:02:56 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,40 @@ async function run(): Promise<void> {
 
     // Get owner and repo from context of payload that triggered the action
     const {owner: currentOwner, repo: currentRepo} = github.context.repo
-
-    // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    const release_id = +core.getInput('id', {required: true})
+   
     const owner = core.getInput('owner', {required: false}) || currentOwner
     const repo = core.getInput('repo', {required: false}) || currentRepo
-
+    
     // Update a release
     // API Documentation: https://developer.github.com/v3/repos/releases/#update-a-release
     // Octokit Documentation: https://octokit.github.io/rest.js/v16#repos-update-release
-    await octokit.rest.repos.updateRelease({
+    const releases = await octokit.rest.repos.listReleases({
       owner,
-      repo,
-      release_id,
-      draft: false
+      repo
     })
 
-    core.info(`Published release with id ${release_id}`)
+    if (releases.data.length === 0) {
+      core.info('Aucune release trouvée.')
+      return
+    }
+
+    core.info(`Nombre de releases trouvées: ${releases.data.length}`)
+
+    for (const release of releases.data) {
+      core.info(`Release: ${release.id} - ${release.name} - ${release.html_url}`)
+      await octokit.rest.repos.updateRelease({
+        owner,
+        repo,
+        release.id,
+        draft: false
+
+      })
+      core.info(`Published release with id ${release.id}`)
+    }
+
+   
+
+  
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
