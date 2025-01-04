5026fd1cfec2ea096b4ab9248180c6024d54dc26
arbinger <thomas.iniguez+github@free.fr>
2025-01-04 13:40:17 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ async function run(): Promise<void> {
 
     // Get owner and repo from context of payload that triggered the action
     const {owner: currentOwner, repo: currentRepo} = github.context.repo
-   
+
     const owner = core.getInput('owner', {required: false}) || currentOwner
     const repo = core.getInput('repo', {required: false}) || currentRepo
-    
+
     // Update a release
     // API Documentation: https://developer.github.com/v3/repos/releases/#update-a-release
     // Octokit Documentation: https://octokit.github.io/rest.js/v16#repos-update-release
@@ -29,20 +29,17 @@ async function run(): Promise<void> {
     core.info(`Nombre de releases trouvées: ${releases.data.length}`)
 
     for (const release of releases.data) {
-      core.info(`Release: ${release.id} - ${release.name} - ${release.html_url}`)
+      core.info(
+        `Release: ${release.id} - ${release.name} - ${release.html_url}`
+      )
       await octokit.rest.repos.updateRelease({
         owner,
         repo,
-        release_id:release.id,
+        release_id: release.id,
         draft: false
-
       })
       core.info(`Published release with id ${release.id}`)
     }
-
-   
-
-  
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
